f44cf476389d8f6865451a3421c3443c4e8c22b3
The Code Rocket <77007514+TheCodeRocket@users.noreply.github.com>
2023-02-17 20:49:46 +0200

    final Submission

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a48faaf8fb44248dbbc641211b50f442-708392878.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
